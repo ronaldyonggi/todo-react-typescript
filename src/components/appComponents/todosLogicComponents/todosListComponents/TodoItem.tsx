3ca923b67b359848f9b064c46e99af1784a28a27
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-19 16:54:49 -0400

    implement functionality to check checkboxes

--- src/components/appComponents/todosLogicComponents/todosListComponents/TodoItem.tsx
+++ src/components/appComponents/todosLogicComponents/todosListComponents/TodoItem.tsx
@@ -1,11 +1,32 @@
 interface TodoItemProps {
   todo: Todo
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
 const TodoItem = (props: TodoItemProps) => {
   const todo = props.todo
+  const setTodos = props.setTodos
+
+  const handleChecked = (id: number) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) return {
+          ...todo,
+          completed: !todo.completed
+        }
+        else return todo
+      }))
+  }
+
   return (
-    <li key={todo.id}>{todo.title}</li>
+    <li>
+      <input 
+      type="checkbox" 
+      checked={todo.completed}
+      onChange={() => handleChecked(todo.id)}
+      />
+      {todo.title}
+    </li>
   )
 }
 
