659d31aa1bca7a8bbd33358098c5f53b1c511d84
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-19 16:54:32 -0400

    pass setTodos to TodoItem as props

--- src/components/appComponents/todosLogicComponents/TodosList.tsx
+++ src/components/appComponents/todosLogicComponents/TodosList.tsx
@@ -2,15 +2,16 @@ import TodoItem from "./todosListComponents/TodoItem";
 
 interface TodosListProps {
   todos: Todo[]
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
 const TodosList = (props: TodosListProps) => {
-  const todos = props.todos
+  const [todos, setTodos] = [props.todos, props.setTodos]
+
   return (
     <ul>
       {todos.map(todo => (
-        // <li key={todo.id}>{todo.title}</li>
-        <TodoItem todo={todo}/>
+        <TodoItem key={todo.id} todo={todo} setTodos={setTodos}/>
       ))}
     </ul>
   )
