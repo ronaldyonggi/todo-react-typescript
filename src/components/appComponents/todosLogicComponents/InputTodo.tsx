5e9001e787b1e9920a57a6c1861b0ac2327e6d39
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-20 16:06:50 -0400

    added functionality to add new todo

--- src/components/appComponents/todosLogicComponents/InputTodo.tsx
+++ src/components/appComponents/todosLogicComponents/InputTodo.tsx
@@ -1,6 +1,40 @@
-const InputTodo = () => {
+import { useState } from "react";
+import {v4 as uuidv4} from "uuid";
+
+
+interface InputTodoProps {
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const InputTodo = (props: InputTodoProps) => {
+  const setTodos = props.setTodos
+  const [title, setTitle] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    }
+    setTodos(prevState =>[...prevState].concat([newTodo]))
+    setTitle('')
+  }
+
   return (
-    <div>Input field here...</div>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      placeholder="Add todo..."
+      value={title}
+      onChange={handleChange}
+       />
+      <button>Submit</button>
+    </form>
   )
 }
 
