51f6a55a796eaaa02659cb7c6d157e0b52aee3d9
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-04-24 14:02:08 -0400

    style sheet extension changed to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './components/styles/app.css'
+import './components/styles/app.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
